6350f9cb01aed861df3fe44cf38235123ebe2bac
Pedro Vitor Silveira Gomes <pedrocrf2002@gmail.com>
2021-10-27 20:39:13 -0300

    wireframe

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const btnMobile = document.querySelector(".mobile_menu");
 
-const toogleMenu = () => {
+const toogleMenu = (e) => {
+  if (e.type === "touchstart") {
+    e.preventDefault();
+  }
   const nav = document.querySelector("#nav");
   nav.classList.toggle("active");
+  
 };
 
 btnMobile.addEventListener("click", toogleMenu);
+btnMobile.addEventListener("touchstart", toogleMenu);
+
